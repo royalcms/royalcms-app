0b9ca76e2dced564d50e5843bb1623314deb765f
王正东 <wangzhengdong@ecmoban.com>
2019-10-21 15:18:12 +0800

    添加应用自动发现命令

---
+++ Royalcms/Component/App/Commands/AppPackageDiscoverCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Royalcms\Component\App\Commands;
+
+use Royalcms\Component\App\AppPackageManifest;
+use Royalcms\Component\Console\Command;
+
+class AppPackageDiscoverCommand extends Command
+{
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'package:app-discover';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Rebuild the cached app package manifest';
+
+    /**
+     * Execute the console command.
+     *
+     * @param  \Royalcms\Component\App\AppPackageManifest  $manifest
+     * @return void
+     */
+    public function handle(AppPackageManifest $manifest)
+    {
+        $manifest->build();
+
+        foreach (array_keys($manifest->manifest) as $package) {
+            $this->line("Discovered App Package: <info>{$package}</info>");
+        }
+
+        $this->info('App Package manifest generated successfully.');
+    }
+}
